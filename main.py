6b98320cc1ac865cbe116f4e6224396706b2f780
Cral0202 <02cral@gmail.com>
2024-02-01 20:24:16 +0100

    Additions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 import sys
 import requests
 from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QFileDialog
+
 from mainGUI import Ui_MainWindow
 
 
@@ -12,6 +15,8 @@ class Main:
         self.clipboard = None
 
         self.user_public_ip = None
+        self.file_to_send = None
+        self.ip_to_send = None
 
     # Sets up the main window
     def window_setup(self):
@@ -33,7 +38,10 @@ class Main:
         self.clipboard = QtWidgets.QApplication.clipboard()
 
         self.ui.ipLabel.setText(f"Your IP: {self.user_public_ip}")
-        self.ui.copyButton.clicked.connect(self.copy_user_id_to_clipboard)
+
+        self.ui.copyButton.clicked.connect(self.copy_user_ip_to_clipboard)
+        self.ui.fileButton.clicked.connect(self.choose_file_to_send)
+        self.ui.ipConfirmButton.clicked.connect(self.choose_ip_to_send)
 
     # Gets the public ip of the user
     def get_user_public_ip(self):
@@ -45,8 +53,23 @@ class Main:
         except requests.RequestException as e:
             return None
 
-    def copy_user_id_to_clipboard(self):
+    # Used to copy the users IP to the clipboard
+    def copy_user_ip_to_clipboard(self):
         self.clipboard.setText(self.user_public_ip)
+
+    # Used to choose which file to send
+    def choose_file_to_send(self):
+        initial_dir = os.getcwd()
+
+        # Returns a tuple where first element is path
+        self.file_to_send = QFileDialog.getOpenFileName(self.window, "Choose file", initial_dir)
+        self.ui.fileLabel.setText(f"Chosen file: {self.file_to_send[0]}")
+
+    # Used to choose which IP to send files to
+    def choose_ip_to_send(self):
+        self.ip_to_send = self.ui.ipLine.text()
+        self.ui.ipToSendLabel.setText(f"Chosen IP: {self.ip_to_send}")
+
 
 if __name__ == "__main__":
     main = Main()
